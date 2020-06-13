481b472d6ed40415980ad88b632d913d0e2e980c
alokkaintura1 <66882294+alokkaintura1@users.noreply.github.com>
2020-06-13 23:30:36 +0530

    commit

---
+++ myfile.php
@@ -0,0 +1,9 @@
+ <center> Task 1 Hybrid Multi Cloud computing Completed </center> 
+<br />
+<pre>
+<?php
+echo'
+<center> <h1> Terraform is an open-source infrastructure as code software tool created by HashiCorp </h1> </center>
+';
+?>
+</pre>
